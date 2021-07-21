9bc78f6e2fcb2ca96e57512d5728668cc0a9ddab
Sam <56245928+syzxy@users.noreply.github.com>
2021-07-22 02:25:05 +1000

    Initial commit

---
+++ src/Slider.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import cards from "./data";
+import { makeStyles } from "@material-ui/styles";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "20px 0",
+    border: "1px solid"
+  },
+  slider: {
+    height: "400px",
+    perspective: "2000px",
+    transformStyle: "preserve-3d",
+    position: "relative"
+  },
+  card: {
+    cursor: "pointer",
+    opacity: 0,
+    position: "absolute",
+    width: "70%",
+    height: "100%",
+    right: 0,
+    left: 0,
+    margin: "0 auto",
+    borderRadius: "10px",
+    overflow: "hidden",
+    transition: "all 0.5s ease-in-out",
+    "&$active": {
+      opacity: 1,
+      boxShadow: "0 0px 30px 5px rgba(0,0,0,0.3)",
+      transform: "translate3d(0,0,0)"
+    },
+    "&$p2": {
+      opacity: 1,
+      boxShadow: "0 1px 4px 0 rgba(0,0,0,.37)",
+      transform: "translate3d(-30%,0,-300px)"
+    },
+    "&$p1": {
+      opacity: 1,
+      boxShadow: "0 6px 10px 0 rgba(0,0,0,.3), 0 2px 2px 0 rgba(0,0,0,.2)",
+      transform: "translate3d(-18%,0,-200px)"
+    },
+    "&$n1": {
+      opacity: 1,
+      boxShadow: "0 6px 10px 0 rgba(0,0,0,.3), 0 2px 2px 0 rgba(0,0,0,.2)",
+      transform: "translate3d(18%,0,-200px)"
+    },
+    "&$n2": {
+      opacity: 1,
+      boxShadow: "0 1px 4px 0 rgba(0,0,0,.37)",
+      transform: "translate3d(30%,0,-300px)"
+    }
+  },
+  active: {},
+  p1: {},
+  p2: {},
+  n1: {},
+  n2: {},
+  image: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    userSelect: "none",
+    pointerEvents: "none"
+  }
+}));
+
+export default function Slider(props) {
+  const classes = useStyles();
+  const [current, setCurrent] = useState(0);
+  const [p1, setP1] = useState();
+  const [p2, setP2] = useState();
+  const [n1, setN1] = useState();
+  const [n2, setN2] = useState();
+  useEffect(() => {
+    const prev = getPrev(current);
+    const next = getNext(current);
+    setP1(prev);
+    setP2(getPrev(prev));
+    setN1(next);
+    setN2(getNext(next));
+  }, [current]);
+  const getNext = (index) => (index + 1) % cards.length;
+  const getPrev = (index) => (index - 1 + cards.length) % cards.length;
+  const indexToClassName = (index) => {
+    if (index === current) return clsx(classes.card, classes.active);
+    if (index === p1) return clsx(classes.card, classes.p1);
+    if (index === p2) return clsx(classes.card, classes.p2);
+    if (index === n1) return clsx(classes.card, classes.n1);
+    if (index === n2) return clsx(classes.card, classes.n2);
+    return classes.card;
+  };
+  // console.log(p2, p1, current, n1, n2);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.slider}>
+        {cards.map((card, i) => (
+          <div
+            key={card.id}
+            className={indexToClassName(i)}
+            onClick={() => setCurrent(i)}
+          >
+            <img className={classes.image} src={card.src} alt="" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const cards = [
+  {
+    id: 1,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    src: "./img1.jpg"
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    src: "./img2.jpg"
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    src: "./img3.jpg"
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    src: "./img4.jpg"
+  },
+  {
+    id: 5,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    src: "./img5.jpg"
+  }
+];
+
+export default cards;
